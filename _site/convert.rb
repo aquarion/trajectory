2ef799d8012eb24581038366694bb514e28901da
Nicholas Avenell <nicholas@istic.net>
2016-10-14 11:47:48 +0200

    Basics

---
+++ _site/convert.rb
@@ -0,0 +1,10 @@
+require 'gollum-lib'
+wiki = Gollum::Wiki.new("../aquarionics-wiki.wiki")
+wiki.pages.each do |p| 
+	puts p.title
+	filename = p.filename.gsub(/md$/, "html")
+
+	text = "--- \nlayout: page\ntitle: #{p.title}\n--- \n#{p.formatted_data}"
+	File.open(filename, 'w') { |file| file.write(text) }
+	#puts p.formatted_data
+end
