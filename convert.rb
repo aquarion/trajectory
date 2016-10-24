8d5d9b86e8aae8d5e808ffd2b9e504258e314f22
Nicholas Avenell <nicholas@istic.net>
2016-10-24 14:14:37 +0200

    Set base path

--- convert.rb
+++ convert.rb
@@ -1,5 +1,5 @@
 require 'gollum-lib'
-wiki = Gollum::Wiki.new("../trajectory.wiki")
+wiki = Gollum::Wiki.new("../trajectory.wiki", :base_path => "/trajectory")
 wiki.pages.each do |p| 
 	puts p.title
 	filename = p.filename.gsub(/md$/, "html")
